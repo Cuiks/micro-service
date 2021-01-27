462b2ad48e1c0b364346fa8a5b89afe76f6afb4b
ganggu <ganggu@newborntown.com>
2021-01-27 22:46:46 +0800

    feat

--- message-rhtift-python-service/message/message_service.py
+++ message-rhtift-python-service/message/message_service.py
@@ -1,4 +1,4 @@
-from message.api import MessageService
+from api import MessageService
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
